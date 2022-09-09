82964213ef6bc3fd6dfbf455214313b04530c752
dthomas91 <davidthomas8891@gmail.com>
2022-09-09 16:10:31 -0700

    first set of answers

--- code-challenges.js
+++ code-challenges.js
@@ -6,23 +6,48 @@
 
 // --------------------INSTRUCTOR EXAMPLE: Create the code that determines which of the two strings has more characters. Use the two sets of test variables provided.
 
-// Describe your process:
+// Describe your process: Create a conditional statement if/if else/else. Add a .length to those conditional statements to determine how many characters each fruit has. In the conditional statement use math (< , >, etc.) to declare if fruit1 is longer than fruit2 to display fruit1 and visa versa for the "else if" statement.
 
 // Set one:
-const fruit1 = "apple"
-const fruit2 = "banana"
+// const fruit1 = "apple"
+// const fruit2 = "banana"
+// if (fruit1.length > fruit2.length) {
+//     console.log(`${fruit1}`)
+// } else if (fruit1.length < fruit2.length) {
+//     console.log(`${fruit2}`)
+// } else {
+//     console.log("Invalid Conditional Statements")
+// }
+
 // Expected outcome: "banana"
 
 // Set two:
-// const fruit1 = "cherry"
-// const fruit2 = "kiwi"
+ const fruit1 = "cherry"
+ const fruit2 = "kiwi"
+
+ if (fruit1.length > fruit2.length) {
+    console.log(`${fruit1}`)
+} else if (fruit1.length < fruit2.length) {
+    console.log(`${fruit2}`)
+} else {
+    console.log("Invalid Conditional Statements")
+}
 // Expected outcome: "cherry"
 
 // --------------------1) Create the code that determines if a given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
-// Describe your process:
+// Describe your process: Create an if statement, us the math format of if temp is less than (<) 212 return "Bellow boiling temp". Creating an else if statement if temp is more than (>) 212 to return "temp is about boiling temp". Again another else if statement of it it is equal (==) to 212 to return "is at boiling temp". You use this code `${temp}` to make it display the temp in the return of your console.log
 
-const temp = 42
+const temp = 212
+if (temp < 212) {
+    console.log (`${temp}`, "is bellow boiling temp")
+} else if (temp > 212) {
+    console.log (`${temp}`, "is above boiling temp")
+} else if (temp == 212) {
+    console.log (`${temp}`, "is at boiling temp")
+} else {
+    console.log("Invalid Conditional Statement")
+}
 // Expected output: "42 is below boiling point"
 
 // const temp = 350
@@ -33,36 +58,49 @@ const temp = 42
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
-// Describe your process:
+// Describe your process: to merge the two const we simply use the command .concat, that will merge all nine of the numbers into one array. After this to count the elements within the array you use the command .length in between the final two ().
 
 const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+
+console.log(padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns).length)
+
 // Expected output: 9
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
-// Describe your process:
+// Describe your process: First I seperated the individual letters of the string by using a .split("") command. Then I reversed all of the individual letters with the .reverse() command and then finished off by bringing all of the letters back together in one string by using .join("")
 
 const currentCohort = "Foxtrot 2022"
+
+console.log(currentCohort.split("").reverse().join(""));
 // Expected output: "2202 tortxoF"
 
 // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
-// Describe your process:
+// Describe your process: Create a console.log for myNumbers with a command of .lastIndexOf(givenValue1) and the same process for givenValue2
 
 const myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
 const givenValue1 = 42
+
+console.log(myNumbers.lastIndexOf(givenValue1))
+
+
 // Expected output: 7
 
 const givenValue2 = 10
+
+console.log(myNumbers.lastIndexOf(givenValue2))
 // Expected output: 8
 
 // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
-// Describe your process:
+// Describe your process: sort the numbers by using the .sort command after the const. Then determining which direction to sort the numbers by using ((a, b) => b - a)) which starts at the highest number and works its way down.
 
 const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
+console.log(sanDiegoSummerTemperatures.sort((a, b) => b - a))
 // Expected output: [82, 80, 79, 77, 76, 73, 72]
 
 const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
+console.log(sanDiegoWinterTemperatures.sort((a, b) => b - a))
 // Expected output: [68, 67, 66, 66, 62, 59, 59]
--- coding-concepts.js
+++ coding-concepts.js
@@ -7,53 +7,53 @@
 
 // --------------------INSTRUCTOR EXAMPLE: What will this log?
 const colors = ["tangerine", "magenta", "lilac", "daffodil"]
-// console.log(colors.push("indigo"))
+console.log(colors.push("indigo"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: It will add indigo and an additional element in the array at the very end of the array after daffodil
+// b) Verify and explain: it displayed 5... I forgot that for it to display indigo after daffodil that I would need to run another console.log(colors)
 
 
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2022"
-// console.log(cohort.length)
+console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: It will log the number of how many indexes in the return which would be 9
+// b) Verify and explain: result came back as 10, I started counting at 0 which is correct for index placement but wrong for how many characters there are.
 
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
-// console.log(greeting[4])
+console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: it will return the letter "o"
+// b) Verify and explain: it is correct because the [4] in the console log after greeting is telling it to return the value of the 4th index, which the index always starts at 0 resulting in the "o" being in the 4th index location
 
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
-// console.log(languages[index])
+console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will return "Ruby"
+// b) Verify and explain: Correct, The index = 1 is telling the code to return the value of what is in index location 1, which javascript it located in index 0 making ruby sit in index 1.
 
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+//console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This code will make the words saturday and sunday to both become uppercased i.e. SATURDAY and SUNDAY
+// b) Verify and explain: Incorrect - doesn't work because the .toUpperCase function only works for a string and ["saturay", "sunday"] is an array. 
 
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: it will display an error because "typeof" is not defined
+// b) Verify and explain: it displays "number". The "typeof" command is changing what the code is trying to return instead of returning the number of elements in the array plus 1. 
